8254fba30299966813104e88de8d22bbe8e0928b
ankitkumargupta <ankitgupta6881@gmail.com>
2024-12-23 12:30:15 +0530

    Learning react 1st repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Secondpage from "./Components/Secondpage";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Secondpage />
+    </div>
+  );
+}
+
+export default App;
